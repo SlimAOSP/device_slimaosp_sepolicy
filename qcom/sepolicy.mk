4d7d953eb4611d361d2f24c632b91845a04bab88
Manish4586 <manish.n.manish45@gmail.com>
2020-01-02 15:03:44 -0500

    introduce SlimAOSP Custom Sepolicy
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from slimaosp
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,14 +10,14 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/slimaosp/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/system
+    device/slimaosp/sepolicy/qcom/dynamic \
+    device/slimaosp/sepolicy/qcom/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/slimaosp/sepolicy/qcom/dynamic \
+    device/slimaosp/sepolicy/qcom/vendor
 endif
